e3b909146b8c7882d2ff6d0b9e4bbfd5dc3d9edb
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2016-08-25 13:46:05 +0200

    converter: main function prototype

---
+++ converter.py
@@ -0,0 +1,8 @@
+"""Routines for bubble to dot conversion"""
+
+
+import os
+
+
+def to_dot(bblfile:str, dotfile:str=None):
+    """Write in dotfile a graph equivalent to those depicted in bubble file"""
