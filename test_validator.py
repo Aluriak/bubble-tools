25c11ad2047edb4669e3775c60b24b2cc0b62dfb
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2016-08-26 14:57:03 +0200

    validator: functionnal tests for empty powernodes

--- test_validator.py
+++ test_validator.py
@@ -3,8 +3,25 @@
 import validator
 
 
-def test_validate():
-    pass
+def test_validate_empty_pwn():
+    data = (
+        "EDGE	a	p1	1.0",
+        "SET	p1	1.0",
+    )
+    expected = {
+        "INFO 1 lines of type EDGE",
+        "INFO 1 lines of type SET",
+        "INFO 2 lines of payload",
+        "INFO 2 top (power)nodes",
+        "INFO 1 connected components",
+        "INFO 0 nodes are defined, 1 are used",
+        "INFO 1 powernodes are defined, 1 are used",
+        "WARNING empty powernode: p1 is defined, but contains nothing",
+    }
+    result = tuple(validator.validate(data, profiling=True))
+    assert len(result) == len(expected)
+    assert set(result) == expected
+
 
 
 def test_included():
