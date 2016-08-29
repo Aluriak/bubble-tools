f35f96a3faede2c06fe08808a9ff05f2583760c9
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2016-08-29 17:01:12 +0200

    tests: moar tests about validation

--- bubbletools/test/test_validator.py
+++ bubbletools/test/test_validator.py
@@ -3,6 +3,34 @@
 from bubbletools import validator
 
 
+def test_validate_loop():
+    data = (
+        "IN	a	p1",
+        "IN	p1	p2",
+        "IN	p2	p3",
+        "IN	p3	p1",
+    )
+    expected = {
+        "INFO 4 lines of type IN",
+        "INFO 4 lines of payload",
+        "INFO 0 top (power)nodes",
+        "INFO 0 connected components",
+        "INFO 0 nodes are defined, 1 are used",
+        "INFO 0 powernodes are defined, 3 are used",
+        "ERROR overlapping powernodes",
+        "ERROR overlapping powernodes",
+        "ERROR overlapping powernodes",
+        "WARNING singleton powernode",
+        "WARNING singleton powernode",
+        "ERROR inclusion cycle: the following 4 nodes are involved",
+    }
+    results = tuple(validator.validate(data, profiling=True))
+    # delete the mutable part (order within the sets)
+    results = {result[:result.rfind(':')] if ':' in result else result
+               for result in results}
+    assert set(results) == expected
+
+
 def test_validate_empty_pwn():
     data = (
         "EDGE	a	p1	1.0",
@@ -21,7 +49,6 @@ def test_validate_empty_pwn():
     result = tuple(validator.validate(data, profiling=True))
     assert len(result) == len(expected)
     assert set(result) == expected
-
 
 
 def test_included():
