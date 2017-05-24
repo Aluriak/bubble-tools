45e8f6878ec917a1415d892b309f796646c5469c
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2017-05-24 16:50:05 +0200

    unit test: update + test lastly introduced features

--- bubbletools/test/test_bbltree.py
+++ bubbletools/test/test_bbltree.py
@@ -1,8 +1,10 @@
 
 
+import pytest
 from bubbletools import bbltree
 
-BBL_DATA_TEST = (
+
+BUBBLE_DATA = (
     ('EDGE', 'k', 'p1'),
     ('EDGE', 'k', 'p2'),
     ('EDGE', 'p3', 'p4'),
@@ -21,9 +23,37 @@ BBL_DATA_TEST = (
     ('SET', 'p3'),
 )
 
-def test_from_bubble_data():
-    tree = bbltree.BubbleTree.from_bubble_data(BBL_DATA_TEST)
-    edges, inclusions, roots = tree.edges, tree.inclusions, tree.roots
+
+@pytest.fixture
+def powergraph():
+    return bbltree.BubbleTree.from_bubble_data(BUBBLE_DATA)
+
+@pytest.fixture
+def oriented_powergraph():
+    return bbltree.BubbleTree.from_bubble_data(BUBBLE_DATA, oriented=True)
+
+
+def test_containment_primitives(powergraph):
+    assert not set(powergraph.nodes_in('k'))
+    assert set(powergraph.nodes_in('p2')) == {'h', 'g', 'f', 'e'}
+    assert set(powergraph.powernodes_in('p1')) == {'p3'}
+
+
+def test_powernode_data(powergraph):
+    data = powergraph.powernode_data('p2')
+    assert data.size == 4
+    assert data.contained == {'h', 'g', 'p4', 'f', 'e'}
+    assert data.contained_pnodes == {'p4'}
+    assert data.contained_nodes == {'h', 'g', 'f', 'e'}
+
+
+def test_no_powernode_data(powergraph):
+    with pytest.raises(ValueError):
+        powergraph.powernode_data('k')
+
+
+def test_from_bubble_data(powergraph):
+    edges, inclusions, roots = powergraph.edges, powergraph.inclusions, powergraph.roots
 
     assert edges == {'p2': {'k'}, 'k': {'p2', 'p1'}, 'p3': {'p4'}, 'p1': {'k'}, 'p4': {'p3'}}
     assert inclusions == {'p2': {'h', 'g', 'p4'}, 'p3': {'a', 'b'},
@@ -32,7 +62,7 @@ def test_from_bubble_data():
                           'd': (), 'e': (), 'f': (), 'g': (), 'h': ()}
     assert roots == {'p2', 'k', 'p1'}
 
-    cc, subroots = tree.connected_components()
+    cc, subroots = powergraph.connected_components()
     assert len(cc) == 1
     assert next(iter(cc.values())) == {'p2', 'e', 'd', 'g', 'c', 'b', 'f', 'p3', 'k', 'h', 'a', 'p1', 'p4'}
     assert len(subroots) == 1
@@ -40,10 +70,9 @@ def test_from_bubble_data():
     assert next(iter(subroots.keys())) == next(iter(cc.keys()))
 
 
-def test_from_oriented_bubble_data():
-    # same data, but understood as oriented
-    tree = bbltree.BubbleTree.from_bubble_data(BBL_DATA_TEST, oriented=True)
-    edges, inclusions, roots = tree.edges, tree.inclusions, tree.roots
+def test_from_oriented_bubble_data(oriented_powergraph):
+    powergraph = oriented_powergraph
+    edges, inclusions, roots = powergraph.edges, powergraph.inclusions, powergraph.roots
 
     assert edges == {'k': {'p2', 'p1'}, 'p3': {'p4'}}
     assert inclusions == {'p2': {'h', 'g', 'p4'}, 'p3': {'a', 'b'},
@@ -52,7 +81,7 @@ def test_from_oriented_bubble_data():
                           'd': (), 'e': (), 'f': (), 'g': (), 'h': ()}
     assert roots == {'p2', 'k', 'p1'}
 
-    cc, subroots = tree.connected_components()
+    cc, subroots = powergraph.connected_components()
     assert len(cc) == 1
     assert next(iter(cc.values())) == {'p2', 'e', 'd', 'g', 'c', 'b', 'f', 'p3', 'k', 'h', 'a', 'p1', 'p4'}
     assert len(subroots) == 1
